33ec6fe333d9400d2ca01848f32419d19dfb37cb
Alexandre Cesar <acmorcego@gmail.com>
2019-05-14 23:30:55 -0300

    comment documentations for the methods

--- producer.hpp
+++ producer.hpp
@@ -11,12 +11,52 @@ class Producer {
 
  public:
   Producer();
+
+	/**
+	 * Create a new test
+	 * 
+	 * @param name - test name
+	 */
   void test(std::string name);
+
+	/**
+	 * Generate a passing assertion with an optional message
+	 * 
+	 * @param message - optional message
+	 */
   void pass(std::string message = "");
+
+	/**
+	 * Generate a failing assertion with an optional message
+	 * 
+	 * @param message - optional message
+	 */
   void fail(std::string message = "");
-  void ok(bool isOk, std::string message = "");
+
+
+	/**
+	 * Assert that value is truthy with an optional description message
+	 * 
+	 * @param value - value to be asserted
+	 * @param message - optional description message
+	 */
+  void ok(bool value, std::string message = "");
+
+	/**
+	 * Assert that both values are equal with an optional description message
+	 * 
+	 * @param value - actual value
+	 * @param expected - expected value
+	 * @param message - optional description message
+	 */
   template <class T>
   void equal(T value, T expected, std::string message = "") {}
+
+	/**
+	 * Declare the end of the tests, finalizing the producer's output
+	 * 
+	 * @returns the number of failed tests capped at 254
+	 */
   int end();
 };
 
