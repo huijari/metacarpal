00970f8fb5eb150d4bb6c92149c66282ec66a821
Alexandre Cesar <acmorcego@gmail.com>
2019-05-14 23:38:18 -0300

    implement producer methods

--- producer.hpp
+++ producer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 namespace Metacarpal {
@@ -10,28 +11,37 @@ class Producer {
   unsigned int failed;
 
  public:
-  Producer();
+  Producer() : count(0), failed(0) {}
 
 	/**
 	 * Create a new test
 	 * 
 	 * @param name - test name
 	 */
-  void test(std::string name);
+  void test(std::string name) {
+		std::cout << "# " << name << std::endl;
+	}
 
 	/**
 	 * Generate a passing assertion with an optional message
 	 * 
 	 * @param message - optional message
 	 */
-  void pass(std::string message = "");
+  void pass(std::string message = "") {
+		this->count++;
+		std::cout << "ok " << this->count << " " << message << std::endl;
+	}
 
 	/**
 	 * Generate a failing assertion with an optional message
 	 * 
 	 * @param message - optional message
 	 */
-  void fail(std::string message = "");
+  void fail(std::string message = "") {
+		this->failed++;
+		std::cout << "not ";
+		this->pass(message);
+	}
 
 
 	/**
@@ -40,7 +50,12 @@ class Producer {
 	 * @param value - value to be asserted
 	 * @param message - optional description message
 	 */
-  void ok(bool value, std::string message = "");
+  void ok(bool value, std::string message = "") {
+		if (value)
+			this->pass(message);
+		else
+			this->fail(message);
+	}
 
 	/**
 	 * Assert that both values are equal with an optional description message
@@ -50,14 +65,34 @@ class Producer {
 	 * @param message - optional description message
 	 */
   template <class T>
-  void equal(T value, T expected, std::string message = "") {}
+  void equal(T value, T expected, std::string message = "") {
+    if (value == expected)
+      this->pass(message);
+    else {
+      this->fail(message);
+      std::cout << "  ---" << std::endl;
+      std::cout << "    operator:\tequal" << std::endl;
+      std::cout << "    expected:\t" << expected << std::endl;
+      std::cout << "    actual:\t" << value << std::endl;
+      std::cout << "  ..." << std::endl;
+    }
+	}
 
 	/**
 	 * Declare the end of the tests, finalizing the producer's output
 	 * 
 	 * @returns the number of failed tests capped at 254
 	 */
-  int end();
+  int end() {
+		std::cout << "1.." << this->count << std::endl;
+		std::cout << "# tests " << this->count << std::endl;
+		std::cout << "# pass " << this->count - this->failed << std::endl;
+		std::cout << "# fail " << this->failed << std::endl;
+
+		if (this->failed < 255)
+			return this->failed;
+		return 254;
+	}
 };
 
 }  // namespace Metacarpal
